64fa0dae8bca281d19ae329519b19791d24cd810
Divya Rani <101196300+divy08r@users.noreply.github.com>
2023-09-16 02:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,48 @@
-import streamlit as st
-
-from tensorflow.keras.models import load_model
-import numpy as np
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import os
+from PIL import Image
+import io
 import keras
-from keras.preprocessing import image
-from tensorflow.keras.preprocessing import image
-from PIL import Image, ImageOps
+from keras.models import load_model
+import numpy as np 
+import tensorflow as tf
 
+ 
+app = FastAPI()
 
-st.title("Python or Anaconda Predictor")
-st.header("Large Serpent Classifier")
-st.text("Upload an Image for of either serpent for  image classification as anaconda or python")
-     
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
-uploaded_file2 = st.camera_input('Click image')
+MODEL_PATH = 'network2.h5'
 
+model = load_model(MODEL_PATH)
 output_class = ["battery", "biological", "brown-glass", "cardboard", "clothes", "green-glass", "metal", "paper", "plastic","shoes","trash","white-glass"]
 
-model = load_model('network2.h5')
+def read_file_as_img(data)->np.ndarray:
+    img = np.array(Image.open(io.BytesIO(data)))
+    return img
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    img = read_file_as_img(await file.read())
+    img = tf.image.resize(img, (256, 256))
+    new_image = np.expand_dims(img, 0)
+
+    p1, p2 = model_predict(new_image)
+    return {
+        "prediction": p1,
+        "accuracy": p2
+    }
+
 
 def model_predict(new_image):
-#   t_img = image.load_img(new_image, target_size = (256,256))
-  t_img = ImageOps.fit(new_image, (256, 256), Image.ANTIALIAS)
-
-  t_img = image.img_to_array(t_img)/255
-  t_img = np.expand_dims(t_img, axis=0)
-
-  predicted_array = model.predict(t_img)
+  predicted_array = model.predict(new_image)
   predicted_value = output_class[np.argmax(predicted_array)]
   predicted_accuracy = round(np.max(predicted_array) * 100, 2)
 
   print("Your waste material is ", predicted_value, " with ", predicted_accuracy, " % accuracy")
-  return predicted_value
+  return predicted_value, predicted_accuracy
+ 
+@app.get("/")
+async def read_random_file():
+    return {"hello me"}
 
-if uploaded_file is not None or uploaded_file2 is not None:
-    if uploaded_file2 is not None:
-      image1 = Image.open(uploaded_file2)
-      st.image(image1, caption='Uploaded Image.', use_column_width=True)
-      st.write("Classifying...")
-
-      predicted_value = model_predict(image1)
-      st.write("It predicted " + predicted_value)
-      uploaded_file2=None
-    else:
-      image1 = Image.open(uploaded_file)
-      st.image(image1, caption='Uploaded Image.', use_column_width=True)
-      st.write("Classifying...")
-
-      predicted_value = model_predict(image1)
-      st.write("It predicted " + predicted_value)
-      uploaded_file=None
+app.include_router(ToolsRoutes.router)
